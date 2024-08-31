a4f9f5017316b3dc088a3180e2803493c3b6c7ea
Phan Công Châu <congchau206@gmail.com>
2024-08-31 15:21:13 +0700

    convert

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-var longestPalindrome = function (s) {
-  if (s === "") {
-    return "";
-  }
-  let current = "";
-  let maxLength = 0;
-  let maxReverse = "";
+var convert = function (s, numRows) {
+  if (s.length <= numRows) return s;
+
+  let result = new Array(Math.min(numRows, s.length)).fill("");
+  let row = 0;
+  let node = true;
   for (let i = 0; i < s.length; i++) {
-    for (let j = i; j < s.length + 1; j++) {
-      current = s.slice(i, j);
-      let tempRV = current.split("").reverse().join("");
-      if (current === tempRV) {
-        if (tempRV.length >= maxLength) {
-          maxReverse = current;
-          maxLength = maxReverse.length;
-        }
+    result[row] += s[i];
+
+    if (numRows !== 1) {
+      if (node) {
+        row++;
+      } else {
+        row--;
       }
     }
+    if (row === 0 || row === numRows - 1) {
+      node = !node;
+    }
   }
-  return maxReverse;
+  return result.join("");
 };
 
-console.log(
-  longestPalindrome(
-    "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"
-  )
-);
-// longestPalindrome("babad");
+console.log(convert("AB", 1));
